d61f9979e282b5d52834bae29ed3809257e1520f
Aman <amannagar1357@gmail.com>
2025-07-04 13:04:35 +0530

    env correction

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,8 @@ import messegeRoutes from "./routes/messege.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
-import {app,server} from "./lib/socket.js"
+import {app,server} from "./lib/socket.js";
+
 import path from "path";
 
 dotenv.config();
@@ -20,7 +21,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/messege", messegeRoutes);
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
